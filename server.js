b2fcae78f29d476470b2784a0c55c043732ff3a5
msuhaib01 <warheadforehead123@gmail.com>
2023-12-13 00:38:48 +0500

    setting up server

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3500;
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}...`);
+});
